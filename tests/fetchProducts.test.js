9ddce0a047eff2ec7ea7b2240848df14bde674fb
PedroSantana <phsantana99@gmail.com>
2022-07-11 14:39:10 -0300

    iniciando projeto

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,6 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
+  // iniciando o projeto
   fail('Teste vazio');
 });
