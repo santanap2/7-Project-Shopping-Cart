6aa6bc59924a3879a876ef2bcc448d4d3c2f4840
PedroSantana <phsantana99@gmail.com>
2022-07-21 18:07:25 -0300

    requisito 1 finalizado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,16 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  try {
+    if (!item) throw new Error('You must provide an url');
+    const api = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+    const data = await fetch(api);
+    const json = await data.json();
+    return await json;
+  } catch (error) {
+    return error;
+  }
 };
 
+console.log(fetchProducts('computador'));
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,32 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // iniciando o projeto
-  fail('Teste vazio');
+
+  it('Testa se `fetchProducts` é uma função', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
+
+  it('Testa se fetch é chamado se `fetchProducts` receber o argumento `computador`', async () => {
+    expect.assertions(1);
+    fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  
+  it('Testa se o retorno da função `fetchProducts` retorna erro se não for passado um argumento', async () => {
+    expect.assertions(1);
+    const result = new Error('You must provide an url');
+    expect(await fetchProducts('')).toEqual(result);
+  });
+
+  it('Testa se ao chamar `fetchProducts` com o argumento `computador` a função usa o endpoint correto', async () => {
+    expect.assertions(1);
+    const testAPI = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledWith(testAPI);
+  });
+
+  it('Testa se a `fetchProducts` retorna o esperado', async () => {
+    expect.assertions(1);
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
 });
