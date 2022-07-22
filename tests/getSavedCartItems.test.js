0b45c9f59fc54cdc0e3fd1a909255e43e7c2de0a
PedroSantana <phsantana99@gmail.com>
2022-07-22 00:54:51 -0300

    requisito 7 finalizado

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,11 +4,20 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
 
+  it('Testa se `getSavedCartItems` é uma função', () => {
+    expect(typeof getSavedCartItems).toBe('function');
   });
 
-  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o `cartItems` como parâmetro', () => {
+  it('Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    expect.assertions(1);
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
 
+  it('Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o `cartItems` como parâmetro', () => {
+    expect.assertions(1);
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems')
   });
 });
