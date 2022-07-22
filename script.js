4906924492bac6c12bc7b2505a86b6bc14024183
PedroSantana <phsantana99@gmail.com>
2022-07-21 21:49:09 -0300

    requisito 5 finalizado

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ const createProductItemElement = ({ sku, name, image }) => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+const cartItemClickListener = async (event) => {
+  event.target.remove();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
